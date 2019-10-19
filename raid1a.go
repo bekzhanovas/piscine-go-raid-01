4b3cf819ada2f13acf5b017c499be492c75439b3
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-19 12:47:22 +0600

    Create raid1a.go

---
+++ raid1a.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func Raid1a(x,y int) {
+	z01.PrintRune(111)
+	for i := 1; i <= x - 2; i++ {
+		z01.PrintRune(45)
+	}
+	z01.PrintRune(111)
+	z01.PrintRune(10)
+	
+	for j := 1; j <= y - 2; j++ {
+		z01.PrintRune(124)
+		for i := 1; i <= x - 2; i++ {
+			z01.PrintRune(32)
+		}
+		z01.PrintRune(124)
+		z01.PrintRune(10)
+
+	}
+	
+	z01.PrintRune(111)
+	for i := 1; i <= x - 2; i++ {
+		z01.PrintRune(45)
+	}
+	z01.PrintRune(111)
+	z01.PrintRune(10)
+  }
+  
