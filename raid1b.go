b8e3e05e080f62944b1e5acf8c4deb6d38e16839
Topexan <54759948+Topexan@users.noreply.github.com>
2019-10-19 13:51:14 +0600

    Create raid1b.go

---
+++ raid1b.go
@@ -0,0 +1,53 @@
+package student
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func Raid1b(x, y int) {
+	if y > 1 {
+		if x > 0 {
+			z01.PrintRune('/')
+		}
+		for i := 1; i <= x-2; i++ {
+			z01.PrintRune('*')
+		}
+		if x > 1 {
+			z01.PrintRune('\\')
+		}
+		if x > 0 {
+			z01.PrintRune(10)
+		}
+
+	}
+
+	if y > 1 {
+		if x > 0 {
+			for j := 1; j <= y-2; j++ {
+				z01.PrintRune(rune('*'))
+				for i := 1; i <= x-2; i++ {
+					z01.PrintRune(32)
+				}
+				if x > 1 {
+					z01.PrintRune('*')
+				}
+				z01.PrintRune(10)
+			}
+		}
+	}
+
+	if y >= 1 {
+		if x > 0 {
+			z01.PrintRune(rune('\\'))
+		}
+		for i := 1; i <= x-2; i++ {
+			z01.PrintRune(rune('*'))
+		}
+		if x > 1 {
+			z01.PrintRune(rune('/'))
+		}
+		if x > 0 {
+			z01.PrintRune(10)
+		}
+	}
+}
