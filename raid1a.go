17cf9f9f80139d0480bc5dbb1ff0892a94766b52
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-19 13:29:38 +0600

    Update raid1a.go

--- raid1a.go
+++ raid1a.go
@@ -4,27 +4,52 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func Raid1a(x, y int) {
-	z01.PrintRune(111)
-	for i := 1; i <= x-2; i++ {
-		z01.PrintRune(45)
-	}
-	z01.PrintRune(111)
-	z01.PrintRune(10)
-	for j := 1; j <= y-2; j++ {
-		z01.PrintRune(124)
-		for i := 1; i <= x-2; i++ {
-			z01.PrintRune(32)
-		}
-		z01.PrintRune(124)
-		z01.PrintRune(10)
-	}
+func Raid1a(x,y int) {
 	if y > 1 {
-		z01.PrintRune(111)
-		for i := 1; i <= x-2; i++ {
+		if x > 0 {
+			z01.PrintRune(111)
+		}
+		for i := 1; i <= x - 2; i++ {
 			z01.PrintRune(45)
 		}
-		z01.PrintRune(111)
+		if x > 1 {
+			z01.PrintRune(111)
+	}
+	if x > 0 {
 		z01.PrintRune(10)
 	}
+
+	}	
+
+
+	if y > 1 {
+		if x > 0 {
+			for j := 1; j <= y - 2; j++ {
+			z01.PrintRune(124)
+			for i := 1; i <= x - 2; i++ {
+				z01.PrintRune(32)
+		}
+		if x > 1 {
+			z01.PrintRune(124)
+		}
+		z01.PrintRune(10)
+			}	
+		}
+	}
+
+
+	if y >= 1 {
+		if x > 0 {
+			z01.PrintRune(111)
+		}
+		for i := 1; i <= x - 2; i++ {
+			z01.PrintRune(45)
+		}
+		if x > 1 {
+			z01.PrintRune(111)
+		}
+		if x > 0 {
+			z01.PrintRune(10)
+		}
+	}	
 }
