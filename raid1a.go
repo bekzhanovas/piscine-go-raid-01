4349a82457794570dd2a06784b02089a0fb72bd8
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-19 12:48:42 +0600

    Update raid1a.go

--- raid1a.go
+++ raid1a.go
@@ -4,26 +4,23 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func Raid1a(x,y int) {
+func Raid1a(x, y int) {
 	z01.PrintRune(111)
-	for i := 1; i <= x - 2; i++ {
+	for i := 1; i <= x-2; i++ {
 		z01.PrintRune(45)
 	}
 	z01.PrintRune(111)
 	z01.PrintRune(10)
-	
-	for j := 1; j <= y - 2; j++ {
+	for j := 1; j <= y-2; j++ {
 		z01.PrintRune(124)
-		for i := 1; i <= x - 2; i++ {
+		for i := 1; i <= x-2; i++ {
 			z01.PrintRune(32)
 		}
 		z01.PrintRune(124)
 		z01.PrintRune(10)
-
 	}
-	
 	z01.PrintRune(111)
-	for i := 1; i <= x - 2; i++ {
+	for i := 1; i <= x-2; i++ {
 		z01.PrintRune(45)
 	}
 	z01.PrintRune(111)
