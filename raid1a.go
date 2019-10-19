fcd9017e896b8f94831bb0778ff7a8baad64933a
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-19 12:55:06 +0600

    Update raid1a.go

--- raid1a.go
+++ raid1a.go
@@ -21,8 +21,8 @@ func Raid1a(x, y int) {
 	}
 	if y > 1 {
 		z01.PrintRune(111)
-		for i := 1; i <= x - 2; i++ {
-		z01.PrintRune(45)
+		for i := 1; i <= x-2; i++ {
+			z01.PrintRune(45)
 		}
 		z01.PrintRune(111)
 		z01.PrintRune(10)
